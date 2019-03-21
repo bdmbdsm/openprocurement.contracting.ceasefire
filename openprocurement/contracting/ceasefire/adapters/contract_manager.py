129085d73fc0b196c0fda86f79f944c642e071d3
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-03-21 15:09:48 +0200

    WIP

--- openprocurement/contracting/ceasefire/adapters/contract_manager.py
+++ openprocurement/contracting/ceasefire/adapters/contract_manager.py
@@ -3,6 +3,7 @@ from zope.interface import implementer
 
 from openprocurement.api.utils import validate_with
 from openprocurement.api.utils.data_engine import DataEngine
+from openprocurement.api.utils.ownership import OwnershipOperator
 from openprocurement.contracting.core.interfaces import (
     IContractManager,
 )
@@ -15,6 +16,7 @@ from openprocurement.contracting.ceasefire.adapters.milestone_manager import (
 from openprocurement.contracting.ceasefire.adapters.document_manager import (
     CeasefireContractDocumentManager,
 )
+from openprocurement.contracting.ceasefire.models.schema import Contract
 
 
 @implementer(IContractManager)
@@ -25,8 +27,20 @@ class CeasefireContractManager(object):
     def __init__(self, event):
         self.event = event
         self.document_manager = CeasefireContractDocumentManager
+        self.data_engine = self._data_engine_cls(event)
 
     def create_contract(self):
+        contract = self.data_engine.create_model(Contract)
+        self._add_documents_to_contract(contract)
+
+        ownersip_operator = OwnershipOperator(contract)
+        import ipdb; ipdb.set_trace()
+        acc = ownersip_operator.set_ownership(
+            self.event.auth.user_id, self.event.data.get('transfer_token')
+        )
+        pass
+
+    def _add_documents_to_contract(self, contract):
         for i in self.event.data.get('documents', []):
             doc = type(contract).documents.model_class(i)
             doc.__parent__ = contract
